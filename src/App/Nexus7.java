992a2b3c6b95ae080efbae5a257866fb1bdf55fb
Amy Santos <164592900+starrsilverr@users.noreply.github.com>
2026-08-30 12:18:11 -0600

    Hice pruebas para ver si conecte bien los paquetes

--- src/App/Nexus7.java
+++ src/App/Nexus7.java
@@ -1,10 +1,13 @@
 
 package App;
+import Modelo.Agente; //Aqui llame al paquete agente para que se pueda utilizar aqui, se tiene que hacer lo
+// mismo para todos los paquetes
 
 public class Nexus7 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Agente a = new Agente("susan","UHSJA80","Principienate", 233,3,90,12,45);
+        //a.mostrarDatos(); ESTOS LOS USE COMO PRUEBA
     }
     
 }
