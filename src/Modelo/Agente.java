c16b62f135b70bc2bc69b6c97087bbe19a185347
Amy Santos <164592900+starrsilverr@users.noreply.github.com>
2026-08-30 12:18:44 -0600

    Avance con mi parte usando POO

--- src/Modelo/Agente.java
+++ src/Modelo/Agente.java
@@ -5,4 +5,28 @@ public class Agente {
     private String nombre,codigo, rango;
     private int EXP, rep, energia, estres;
     private double precision;
+
+    //Contructor Parametrizado
+    public Agente(String nombre, String codigo, String rango, int EXP, int rep, int energia, int estres, double precision) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.rango = rango;
+        this.EXP = EXP;
+        this.rep = rep;
+        this.energia = energia;
+        this.estres = estres;
+        this.precision = precision;
+    }
+    
+    //Funcion para mostrar la informacion
+    public void mostrarDatos (){
+    System.out.println("Nombre : " + nombre);
+    System.out.println("Codigo : " + codigo);
+    System.out.println("Rango : " + rango);
+    System.out.println("EXP : " + EXP);
+    System.out.println("Reputacion : " + rep);
+    System.out.println("Energia : " + energia);
+    System.out.println("Estres : " + estres);
+    System.out.println("Preciscion : " + precision);
+}
 }
