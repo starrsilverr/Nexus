d1f0d4fb160a51d562dcc128e45764f40b847244
Amy Santos <164592900+starrsilverr@users.noreply.github.com>
2026-09-04 16:16:53 -0600

    Actualizacion de Jared

--- src/Modelo/Evidencia.java
+++ src/Modelo/Evidencia.java
@@ -1,7 +1,64 @@
 
 package Modelo;
 
-
 public class Evidencia {
-    
+
+    private int id;
+    private String tipo;
+    private String descripcion;
+    private String ubicacion;
+    private boolean analizada;
+    private boolean relevante;
+    private int valor;
+
+    public Evidencia(int id, String tipo, String descripcion, String ubicacion,
+                     boolean analizada, boolean relevante, int valor) {
+
+        this.id = id;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.ubicacion = ubicacion;
+        this.analizada = analizada;
+        this.relevante = relevante;
+        this.valor = valor;
+    }
+
+    public void mostrarEvidencia() {
+
+        System.out.println("ID: " + id);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Descripcion: " + descripcion);
+        System.out.println("Ubicacion: " + ubicacion);
+        System.out.println("Analizada: " + analizada);
+        System.out.println("Relevante: " + relevante);
+        System.out.println("Valor: " + valor);
+    }
+
+    public void analizar() {
+        analizada = true;
+    }
+
+    public void cambiarRelevancia(boolean nuevaRelevancia) {
+        relevante = nuevaRelevancia;
+    }
+
+    public void cambiarValor(int nuevoValor) {
+        valor = nuevoValor;
+    }
+
+    public boolean isAnalizada() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    public int getValor() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    public boolean isRelevante() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    public void setAnalizada(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
--- src/logica/Investigacion.java
+++ src/logica/Investigacion.java
@@ -1,6 +1,81 @@
 
 package logica;
 
+import Modelo.Caso;
+import Modelo.Evidencia;
+import Modelo.Sospechoso;
+
 public class Investigacion {
-    
+
+    private Caso caso;
+    private int puntosSospechoso1;
+    private int puntosSospechoso2;
+    private int evidenciasAnalizadas;
+
+    public Investigacion(Caso caso) {
+
+        this.caso = caso;
+
+        puntosSospechoso1 = 0;
+        puntosSospechoso2 = 0;
+        evidenciasAnalizadas = 0;
+    }
+
+    public void analizarEvidencia(int idEvidencia, int idSospechoso) {
+
+        Evidencia evidencia = caso.buscarEvidencia(idEvidencia);
+
+        if (evidencia != null) {
+
+            if (evidencia.isAnalizada() == false) {
+
+                evidencia.setAnalizada(true);
+
+                evidenciasAnalizadas++;
+
+                if (evidencia.isRelevante()) {
+
+                    if (idSospechoso == 1) {
+                        puntosSospechoso1 = puntosSospechoso1 + evidencia.getValor();
+                    }
+
+                    if (idSospechoso == 2) {
+                        puntosSospechoso2 = puntosSospechoso2 + evidencia.getValor();
+                    }
+                }
+            }
+        }
+    }
+
+    public void mostrarPuntos(int idSospechoso) {
+
+        if (idSospechoso == 1) {
+            System.out.println("Puntos del sospechoso 1: " + puntosSospechoso1);
+        }
+
+        if (idSospechoso == 2) {
+            System.out.println("Puntos del sospechoso 2: " + puntosSospechoso2);
+        }
+    }
+
+    public void mostrarEvidenciasAnalizadas() {
+
+        System.out.println("Evidencias analizadas: " + evidenciasAnalizadas);
+    }
+
+    public void mostrarSospechosoMasProbable() {
+
+        if (puntosSospechoso1 > puntosSospechoso2) {
+
+            System.out.println("El sospechoso 1 tiene más puntos.");
+
+        } else if (puntosSospechoso2 > puntosSospechoso1) {
+
+            System.out.println("El sospechoso 2 tiene más puntos.");
+
+        } else {
+
+            System.out.println("Los dos sospechosos tienen los mismos puntos.");
+        }
+    }
 }
