57f392c4631976ad5c5dea901d420093074630c8
alvar <alvar@AlvaroEnrique>
2026-09-05 12:18:43 -0600

    Prueba4

--- src/Modelo/Agente.java
+++ src/Modelo/Agente.java
@@ -34,7 +34,7 @@ public class Agente {
     public void Rangos(){
         //Ciclo para saber los rango entorno a la puntiacion.
         //nose chavos
-        //PRUEBA3
+        //PRUEBA4
     }
     
 }
